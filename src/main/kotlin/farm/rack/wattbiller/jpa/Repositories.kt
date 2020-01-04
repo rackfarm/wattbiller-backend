bfbdde8afb4a8330e3a49ee0a5da71ac3d8e1d59
Florian  Bürgi <florian-buergi@iet-gibb.ch>
2020-01-04 13:36:36 +0100

    add repositories

--- src/main/kotlin/farm/rack/wattbiller/jpa/Repositories.kt
+++ src/main/kotlin/farm/rack/wattbiller/jpa/Repositories.kt
@@ -1,6 +1,15 @@
 package farm.rack.wattbiller.jpa
 
-import farm.rack.wattbiller.model.User
+import farm.rack.wattbiller.model.*
+import farm.rack.wattbiller.model.meter.Meter
+import farm.rack.wattbiller.model.meter.MeterReading
 import io.micronaut.data.repository.CrudRepository
 
 interface UserRepository: CrudRepository<User, String>
+interface MeterRepository: CrudRepository<Meter, Long>
+interface MeterReadingRepository: CrudRepository<MeterReading, Long>
+interface BillRepository: CrudRepository<Bill, Long>
+interface BillingPeriodRepository: CrudRepository<BillingPeriod,Long>
+interface DebitorGroupRepository: CrudRepository<DebitorGroup, Long>
+interface DebitorGroupMembershipRepository: CrudRepository<DebitorGroupMembership, Long>
+interface UserBillRepository: CrudRepository<UserBill, Long>
