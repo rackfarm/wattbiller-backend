58ba816b17bdff2d4474dc9a931f36be75d859c3
Florian  Bürgi <florian-buergi@iet-gibb.ch>
2020-01-04 23:20:55 +0100

    add repositories

--- src/main/kotlin/farm/rack/wattbiller/model/UserBill.kt
+++ src/main/kotlin/farm/rack/wattbiller/model/UserBill.kt
@@ -2,7 +2,10 @@ package farm.rack.wattbiller.model
 
 import javax.persistence.Entity
 import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
 
 @Entity
-data class UserBill(@ManyToOne val user: User = User()/*, val billingPeriod: BillingPeriod = BillingPeriod() */) :AbstractEntity() {
+data class UserBill(@ManyToOne val user: User = User(),
+                    @ManyToOne val billingPeriod: BillingPeriod = BillingPeriod(),
+                    @OneToMany val bills: List<Bill> = arrayListOf()) :AbstractEntity() {
 }
